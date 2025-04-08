a99b53b9e2c10395aef84815bca5d19a26fc5759
jayn95 <jill.navarra@wvsu.edu.ph>
2025-04-08 08:49:21 +0800

    fixed path for model.tflite

--- navarra/app.py
+++ navarra/app.py
@@ -19,6 +19,9 @@ genai.configure(api_key=GEMINI_API_KEY)
 
 app = Flask(__name__)
 
+# Base directory of the current script
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 def load_and_preprocess_image(img_path, input_shape):
     """Load and preprocess an image for the model."""
     img = Image.open(img_path).convert('RGB')
@@ -95,8 +98,7 @@ def predict():
     file.save(img_path)
 
     # Load the TFLite model and allocate tensors
-    model_path = r"hello-ml\navarra\model\model.tflite"
-
+    model_path = os.path.join(BASE_DIR, "hello-ml", "navarra", "model", "model.tflite")
 
     interpreter = tf.lite.Interpreter(model_path=model_path)
     interpreter.allocate_tensors()
